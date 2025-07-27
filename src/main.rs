a0e55b361c4d159ab88804d40649284dd45ff9af
jivers <john.ivers@gmail.com>
2025-07-26 20:40:07 -0700

    make chat interactive

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use std::env;
 use anyhow::Result;
+use inquire::Text;
 use chat::Chat;
 pub mod chat;
 
-fn main() -> Result<()> {
-    let input = match env::args().nth(1) {
-        Some(x) => x,
-        None => "gimme the beat boy".to_string(),
-    };
-
-    let mut chat = Chat::new("gpt-4o"); 
-    let response = chat.send(&input)?;
+fn get_response(chat: &mut Chat, prompt: String) -> Result<String> {
+    let response = chat.send(&prompt)?;
     let message = response.first()?;
+    Ok(message.content.clone())
+}
 
-    termimad::print_inline(&message.content);
+fn main() -> Result<()> {
+    let mut chat = Chat::new("gpt-4o"); 
+    let prompt = env::args().collect::<Vec<String>>()[1..].join(" "); 
+    if prompt.len() > 0 {
+        let prompt_display = format!("
+```
+{}
+```
 
-    Ok(())
+", prompt);
+        termimad::print_text(&prompt_display);
+
+        let response = get_response(&mut chat, prompt)?;
+        termimad::print_text(&response);
+    }
+
+    loop {
+        let prompt = Text::new("Prompt:").prompt()?;
+        let response = get_response(&mut chat, prompt)?; 
+    
+        termimad::print_text(&response);
+    }
 }
