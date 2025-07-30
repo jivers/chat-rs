64b263903e0c78875df98f39e3bbcf89a3007cfb
jivers <john.ivers@gmail.com>
2025-07-29 20:30:46 -0700

    handle pipe input

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::env;
 use anyhow::Result;
 use inquire::Text;
 use chat::{Chat, Message, Role};
-use assistant::{Assistant, Action};
+use assistant::{Action};
+use atty::Stream;
+use std::io::{self, Read};
+use termimad::print_text;
 
 pub mod chat;
 pub mod assistant;
@@ -14,33 +17,31 @@ fn get_response(chat: &mut Chat, prompt: String) -> Result<String> {
 }
 
 fn main() -> Result<()> {
-    let mut chat = Chat::new("o4-mini");
-    chat.add_message(Message::new(Role::Developer, "you generate only ffmpeg commands and nothing else")); 
-
-    let mut action = Action { 
-        chat,
-        post : Some(Box::new(|mut s: String| {
-            s = s.replace("```", "");
-            if let Some(newline_index) = s.find('\n') {
-                s = (&s[newline_index + 1..]).to_string();
-            }
-            s.trim().to_string()
-        })),
-    };
-
-    let result = action.run("convert test.wav which is a 5.1 dolby 48khz file to a stereo 96 kbpsto test.mp3")?;
-    println!("{}", result);
-
+    let mut prompt = String::new();
     let mut chat = Chat::new("gpt-4o");
-    let args: Vec<String> = env::args().skip(1).collect();
 
-    if args.len() > 0 {
-        let prompt = args.join(" ");
-        let response = get_response(&mut chat, prompt)?;
-        termimad::print_text(&response);
-        return Ok(());
-    }
-    
+    // handle pipe input
+    if atty::is(Stream::Stdin) {
+        let args: Vec<String> = env::args().skip(1).collect();
+        if args.len() > 0 {
+            prompt = args.join(" ");
+            let response = get_response(&mut chat, prompt)?;
+            print_text(&response);
+            return Ok(());
+        }
+    }; 
+
+
+    // handle stdin
+    if atty::isnt(Stream::Stdin) {
+         io::stdin().read_to_string(&mut prompt)?;
+         let response = get_response(&mut chat, prompt)?;
+         print_text(&response); 
+         return Ok(())
+     };
+
+
+    // handle interactive chat
     loop {
         let prompt = Text::new("Prompt:").prompt()?;
         let response = get_response(&mut chat, prompt)?; 
