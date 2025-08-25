501b47d7ad889daca58980d5c71781fb647ca000
jivers <john.ivers@gmail.com>
2025-08-24 20:56:10 -0700

    remove pipe and args as input

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use std::env;
 use std::collections::HashMap;
 use anyhow::Result;
 use inquire::Text;
 use chat::{Chat};
-use atty::Stream;
 use tool::ToolType;
-use std::io::{self, Read};
-use termimad::print_text;
 use function::{Function, Parameters, Property, JsonType};
 use tool::Tool;
 
@@ -23,7 +19,7 @@ fn main() -> Result<()> {
         r#enum: Some(vec!["ffmpeg".into(), "ls".into()]),
     });
 
-    let tool_choose = Tool {
+    let tool_chooser = Tool {
         r#type: ToolType::Function,
         function: Some(Function {
             name: "choose_tool".to_string(),
@@ -37,40 +33,10 @@ fn main() -> Result<()> {
     };
 
     let mut chat = Chat::new("gpt-4o");
-    chat.add_tool(tool_choose);
+    chat.add_tool(tool_chooser);
 
-    let args: Vec<String> = env::args().skip(1).collect();
-    let is_pipe = !atty::is(Stream::Stdin);
-    let has_args = args.len() > 0; 
-
-    // todo: this could get cleaned up
-    match (is_pipe, has_args) {
-        // pipe with prompt
-        (true, true) => {
-            let mut buffer = String::new();
-            io::stdin().read_to_string(&mut buffer)?;
-
-            let prompt = format!("{} \n --- \n {}", buffer, args.join(" "));
-            return chat.complete(prompt)
-        },
-        // pipe no prompt
-        (true, false) => {
-            let mut prompt = String::new();
-            io::stdin().read_to_string(&mut prompt)?;
-
-            return chat.complete(prompt)
-        },
-        // just prompt (as args)
-        (false, true) => {
-            let prompt = args.join(" ");
-            return chat.complete(prompt)
-        },
-        // interactive chat mode
-        _ => {
-            loop {
-                let prompt = Text::new("Prompt:").prompt()?;
-                chat.complete(prompt)?;
-            }
-        }
+    loop {
+        let prompt = Text::new("Prompt:").prompt()?;
+        chat.complete(prompt)?;
     }
 }
